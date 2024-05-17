bd5edf92ce3f6281e4ecd15f35c1e3dcaa18f15a
Mkhuseli Mkeyiya <mkhuseli@databalk.nu>
2024-05-17 18:12:45 +0200

    Feat: New Zaken components

---
+++ src/components/(services)/Zaken/Filte.tsx
@@ -0,0 +1,249 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Switch, TextInput, Modal, Platform } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+const FilterComponent = ({ onClose }) => {
+  const [verzoekChecked, setVerzoekChecked] = useState(false);
+  const [insidentChecked, setInsidentChecked] = useState(false);
+  const [kritiekChecked, setKritiekChecked] = useState(false);
+  const [hoogChecked, setHoogChecked] = useState(false);
+  const [normaalChecked, setNormaalChecked] = useState(false);
+  const [laagChecked, setLaagChecked] = useState(false);
+  const [nieuwChecked, setNieuwChecked] = useState(false);
+  const [inBehandelingChecked, setInBehandelingChecked] = useState(false);
+  const [gereedChecked, setGereedChecked] = useState(false);
+  const [melder, setMelder] = useState(null);
+  const [melderOpen, setMelderOpen] = useState(false);
+  const [melderItems, setMelderItems] = useState([
+    { label: 'Jos Balk', value: 'jos_balk' },
+    { label: 'Martine Naiber', value: 'john_doe' },
+  ]);
+  const [gemaaktOp, setGemaaktOp] = useState<Date>(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [inDeWacht, setInDeWacht] = useState(false);
+
+  const onDateChange = (event, selectedDate) => {
+    const currentDate = selectedDate || gemaaktOp;
+    setShowDatePicker(Platform.OS === 'ios');
+    setGemaaktOp(currentDate);
+  };
+
+  return (
+    <Modal visible={true} animationType="slide" transparent>
+    <View style={styles.container}>
+         <TouchableOpacity onPress={onClose} style={styles.closeButton}>
+          <Text style={styles.closeButtonText}>X</Text>
+        </TouchableOpacity>
+      <Text style={styles.title}>Filter</Text>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Type</Text>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setVerzoekChecked(!verzoekChecked)} style={styles.checkbox}>
+            {verzoekChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Verzoek</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setInsidentChecked(!insidentChecked)} style={styles.checkbox}>
+            {insidentChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Incident</Text>
+        </View>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Prioriteit</Text>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setKritiekChecked(!kritiekChecked)} style={styles.checkbox}>
+            {kritiekChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Kritiek</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setHoogChecked(!hoogChecked)} style={styles.checkbox}>
+            {hoogChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Hoog</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setNormaalChecked(!normaalChecked)} style={styles.checkbox}>
+            {normaalChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Normaal</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setLaagChecked(!laagChecked)} style={styles.checkbox}>
+            {laagChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Laag</Text>
+        </View>
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Status</Text>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setNieuwChecked(!nieuwChecked)} style={styles.checkbox}>
+            {nieuwChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Nieuw</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setInBehandelingChecked(!inBehandelingChecked)} style={styles.checkbox}>
+            {inBehandelingChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>In behandeling</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity onPress={() => setGereedChecked(!gereedChecked)} style={styles.checkbox}>
+            {gereedChecked && <View style={styles.checkmark} />}
+          </TouchableOpacity>
+          <Text style={styles.checkboxLabel}>Gereed</Text>
+        </View>
+      </View>
+      <View style={styles.section}>
+      <Text style={styles.sectionTitle}>Melder</Text>
+          <DropDownPicker
+            open={melderOpen}
+            value={melder}
+            items={melderItems}
+            setOpen={setMelderOpen}
+            setValue={setMelder}
+            setItems={setMelderItems}
+            searchable={true}
+            placeholder="Selecteer melder"
+            // style={styles.dropdown}
+            // dropDownContainerStyle={styles.dropdownContainer}
+          />
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Gemaakt op</Text>
+        <TouchableOpacity onPress={() => setShowDatePicker(true)} style={styles.dateInput}>
+            <Text style={styles.dateText}>
+              {gemaaktOp.toLocaleDateString('nl-NL', {
+                day: '2-digit',
+                month: 'long',
+                year: 'numeric',
+              })}
+            </Text>
+          </TouchableOpacity>
+          {showDatePicker && (
+            <DateTimePicker
+              value={gemaaktOp}
+              mode="date"
+              display="default"
+              onChange={onDateChange}
+              minimumDate={new Date(2020, 0, 1)}
+              maximumDate={new Date(2030, 11, 31)}
+            />
+          )}
+      </View>
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>In de wacht</Text>
+        <View style={styles.switchContainer}>
+          <Switch
+            value={inDeWacht}
+            onValueChange={setInDeWacht}
+            trackColor={{ true: '#1D589789', false: '#CCCCCC' }}
+            thumbColor="#FFFFFF"
+          />
+          <Text style={styles.switchLabel}>{inDeWacht ? 'Ja' : 'Nee'}</Text>
+        </View>
+      </View>
+      <TouchableOpacity onPress={onClose} style={styles.submitButton}>
+        <Text style={styles.submitButtonText}>Indienen</Text>
+      </TouchableOpacity>
+    </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'rgba(29, 88, 151, 0.8)',
+        paddingTop: 50,
+        paddingBottom: 50,
+        paddingHorizontal: 20,
+      },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
+  },
+  section: {
+    marginBottom: 15,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 5,
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  checkmark: {
+    width: 12,
+    height: 12,
+    borderRadius: 2,
+    backgroundColor: 'white',
+  },
+  checkboxLabel: {
+    color: 'white',
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: 'white',
+    borderRadius: 4,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: 'black',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  switchLabel: {
+    color: 'white',
+    marginLeft: 10,
+  },
+  submitButton: {
+    width: 70,
+    height: 30,
+    backgroundColor: "white",
+  },
+  submitButtonText: {
+
+  },
+  closeButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  dateInput: {
+    backgroundColor: 'white',
+    borderRadius: 4,
+    padding: 10,
+  },
+  dateText: {
+    color: 'black',
+  },
+});
+export default FilterComponent;
--- src/components/(services)/Zaken/ZakenForm.tsx
+++ src/components/(services)/Zaken/ZakenForm.tsx
@@ -1,92 +1,115 @@
-// ZakenForm.tsx
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet, Modal, TouchableOpacity, Text } from 'react-native';
+import { CheckBox, Slider } from 'react-native-elements';
 
 interface ZakenFormProps {
+  visible: boolean;
+  setVisible: (visible: boolean) => void;
   onSubmit: (formData: any) => void;
 }
 
-const ZakenForm: React.FC<ZakenFormProps> = ({ onSubmit }) => {
+const ZakenForm: React.FC<ZakenFormProps> = ({ visible, setVisible, onSubmit }) => {
   const [formData, setFormData] = useState({
-    type: '',
-    gemaaktOp: '',
-    licentie: '',
-    prioriteit: '',
-    fibo: '',
-    inDeWacht: '',
-    melder: '',
     onderwerp: '',
+    beschrijving: '',
+    prioriteit: '',
+    type: '',
+    uwKenmerk: '',
+    reproduceerbaar: false,
   });
-  const [visible, setVisible] = useState(false); // Start with modal closed
 
-  const handleInputChange = (key: string, value: string) => {
+  const handleInputChange = (key: string, value: string | boolean) => {
     setFormData({ ...formData, [key]: value });
   };
 
   const handleSubmit = () => {
     onSubmit(formData);
-    // Reset form data after submission
     setFormData({
-      type: '',
-      gemaaktOp: '',
-      licentie: '',
-      prioriteit: '',
-      fibo: '',
-      inDeWacht: '',
-      melder: '',
       onderwerp: '',
+      beschrijving: '',
+      prioriteit: '',
+      type: '',
+      uwKenmerk: '',
+      reproduceerbaar: false,
     });
-  };
-
-  const handleClose = () => {
     setVisible(false);
   };
 
-  useEffect(() => {
-    console.log('Form visibility:', visible);
-  }, [visible]);
-
   return (
-    <Modal visible={visible} animationType="slide">
-      <View style={styles.modalContainer}>
-        <View style={styles.modalHeader}>
-          <Text style={styles.modalHeaderText}>Add Zaken Item</Text>
-          <TouchableOpacity onPress={handleClose}>
-            <Text style={styles.closeButton}>Close</Text>
+    <Modal visible={visible} animationType="slide" transparent>
+      <View style={styles.overlay}>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalHeaderText}>Maak Zaak</Text>
+            <TouchableOpacity onPress={() => setVisible(false)}>
+              <Text style={styles.closeButton}>X</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Onderwerp*</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Type here..."
+              placeholderTextColor="white"
+              onChangeText={(text) => handleInputChange('onderwerp', text)}
+              value={formData.onderwerp}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Beschrijving*</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Type here..."
+              placeholderTextColor="white"
+              onChangeText={(text) => handleInputChange('beschrijving', text)}
+              value={formData.beschrijving}
+              multiline
+            />
+          </View>
+          <View style={styles.sliderContainer}>
+            <Text style={styles.sliderLabel}>Prioriteit*</Text>
+            <Slider
+              style={styles.slider}
+              minimumValue={0}
+              maximumValue={100}
+              step={1}
+              onValueChange={(value) => handleInputChange('prioriteit', value.toString())}
+              value={parseInt(formData.prioriteit, 10) || 0}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Type*</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Type here..."
+              placeholderTextColor="white"
+              onChangeText={(text) => handleInputChange('type', text)}
+              value={formData.type}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Uw Kenmerk*</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Type here..."
+              placeholderTextColor="white"
+              onChangeText={(text) => handleInputChange('uwKenmerk', text)}
+              value={formData.uwKenmerk}
+            />
+          </View>
+          <View style={styles.checkboxContainer}>
+            <Text style={styles.checkboxLabel}>Reproduceerbaar:</Text>
+            <CheckBox
+              checked={formData.reproduceerbaar}
+              onPress={() => handleInputChange('reproduceerbaar', !formData.reproduceerbaar)}
+              containerStyle={styles.checkbox}
+              textStyle={{ color: 'white' }}
+            />
+          </View>
+          <TouchableOpacity style={styles.uploadButton}>
+            <Text style={styles.uploadButtonText}>Upload hier</Text>
           </TouchableOpacity>
-        </View>
-        <View style={styles.formContainer}>
-          <TextInput
-            style={styles.input}
-            placeholder="Type"
-            onChangeText={(text) => handleInputChange('type', text)}
-            value={formData.type}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Gemaakt op"
-            onChangeText={(text) => handleInputChange('gemaaktOp', text)}
-            value={formData.gemaaktOp}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Licentie"
-            onChangeText={(text) => handleInputChange('licentie', text)}
-            value={formData.licentie}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Prioriteit"
-            onChangeText={(text) => handleInputChange('prioriteit', text)}
-            value={formData.prioriteit}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Onderwerp"
-            onChangeText={(text) => handleInputChange('onderwerp', text)}
-            value={formData.onderwerp}
-          />
-          <Button title="Submit" onPress={handleSubmit} />
+          <Button title="Indienen" onPress={handleSubmit} color="#1D5897" />
         </View>
       </View>
     </Modal>
@@ -94,11 +117,18 @@ const ZakenForm: React.FC<ZakenFormProps> = ({ onSubmit }) => {
 };
 
 const styles = StyleSheet.create({
-  modalContainer: {
+  overlay: {
     flex: 1,
-    backgroundColor: 'white',
-    paddingHorizontal: 20,
-    paddingTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(29, 88, 151, 0.6)', // Semi-transparent background
+  },
+  modalContainer: {
+    backgroundColor: 'rgba(29, 88, 151, 0.6)', // Same background color as overlay
+    borderRadius: 10,
+    padding: 20,
+    height: '90%',
+    width: '90%',
   },
   modalHeader: {
     flexDirection: 'row',
@@ -109,20 +139,61 @@ const styles = StyleSheet.create({
   modalHeaderText: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'white',
   },
   closeButton: {
     fontSize: 16,
-    color: 'blue',
+    color: 'white',
   },
-  formContainer: {
-    flex: 1,
+  inputContainer: {
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 16,
+    color: 'white',
+    marginBottom: 5,
   },
   input: {
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: 'white',
+    backgroundColor: 'rgba(29, 88, 151, 0.6)',
     padding: 10,
-    marginBottom: 10,
     borderRadius: 5,
+    color: 'white',
+  },
+  sliderContainer: {
+    marginBottom: 20,
+  },
+  sliderLabel: {
+    fontSize: 16,
+    color: 'white',
+    marginBottom: 5,
+  },
+  slider: {
+    width: '100%',
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  checkboxLabel: {
+    marginRight: 10,
+    color: 'white',
+  },
+  checkbox: {
+    backgroundColor: 'transparent',
+    borderColor: 'white',
+  },
+  uploadButton: {
+    backgroundColor: 'lightgray',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+  },
+  uploadButtonText: {
+    textAlign: 'center',
+    color: '#1D5897',
   },
 });
 
